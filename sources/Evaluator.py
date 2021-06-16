757cbd954c039fdb391dd79376a8963b93afebf1
Shinya Sato <shinnya.sato@gmail.com>
2021-06-16 21:37:05 +0900

    負値は文法エラーにした

--- sources/Evaluator.py
+++ sources/Evaluator.py
@@ -211,7 +211,6 @@ precedence = (
     ('left', 'AND','OR'),
     ('left', 'PLUS','MINUS'),
     ('left', 'TIMES','DIVIDE', 'MOD'),
-#    ('right', 'UMINUS'),
 #    ('nonassoc', 'IFX','ELSE'),
     )
 
@@ -440,12 +439,6 @@ def p_expression_builtin(t):
 def p_expression_call(t):
     'expression : NAME print_param'
     t[0] = Node("call", t[1], t[2], lineno=t.lineno(1))
-
-    
-#def p_expression_uminus(t):
-#    'expression : MINUS expression %prec UMINUS'
-#    #t[0] = -t[2]
-#    t[0] = Node("singleop", "-", [t[1]], lineno=t.lineno(1))
 
     
 def p_expression_not(t):
